2dc03da5ef73c648aefbd114c91ca4576097c423
Yago Allàm <yagoallam@gmail.com>
2022-07-31 16:33:01 +0200

    commenti

--- ProgettoSett2/src/it/epicode/catalogo/ArchiveMain.java
+++ ProgettoSett2/src/it/epicode/catalogo/ArchiveMain.java
@@ -9,46 +9,37 @@ import java.util.stream.Collectors;
 
 import org.apache.commons.io.FileUtils;
 
-
 public class ArchiveMain {
 
-//	private static final Logger Log = LoggerFactory.getLogger(ArchiveMain.class);
-	
+ // nel main possiamo fare alcune prove per provare le funzioni:
 
 	public static void main(String[] args) throws IOException {
-		
-	   Map<String, OggettoLibreria> mainCatalog = new Service().popolaLista() ;
-	   
-	   Search s = new SearchClass(mainCatalog);
-	   ScriviLeggi wR = new WriteReadClass(mainCatalog);
 
-		System.out.println("CATALOGO INIZIALE COMPLETO:");
+		Map<String, OggettoLibreria> mainCatalog = new Service().listaMia();
+
+		Search s = new SearchClass(mainCatalog);
+		ScriviLeggi wR = new WriteReadClass(mainCatalog);
+
+		System.out.println("Raccolta del catalogo");
 		mainCatalog.forEach((k, v) -> System.out.println(v));
 		System.out.println("----------------------------");
 		System.out.println("");
 
-		System.out.println("RICERCA PER ANNO (2012):");
-		
-		
+		System.out.println("RICERCA PER ANNO (2018):");
+
 		System.out.println("RICERCA PER AUTORE (Autore 1):");
-		Map<String,OggettoLibreria> filteredCatalog1 = s.searchForAuthor("Autore 1");
-		
-		
-		filteredCatalog1.forEach((k,v)->System.out.println(k + " " +v));
-		System.out.println("------eccoci---------");
-		
-		wR.write();
-		System.out.println("-----------------ciao-----------");
-		List<String> cioao= wR.read(); 
-		cioao.forEach(System.out::println);
+		Map<String, OggettoLibreria> filteredCatalog1 = s.searchForAuthor("Autore 1", mainCatalog );
+
+		filteredCatalog1.forEach((k, v) -> System.out.println(k + " " + v));
+		System.out.println("---------------");
+
+		wR.scriviFileEsterno();
+		System.out.println("-----------------LEGGIAMO QUALCOSA-----------");
+		List<String> prova = wR.leggiFileEsterno();
+		prova.forEach(System.out::println);
 
 		
-		System.out.println("");
-
-		//saveOnDisk();
-
-		System.out.println("LETTURA DOPO SCRITTURA SU DISCO:");
-		//loadFromDisk();
+		
 
 	}
 }
